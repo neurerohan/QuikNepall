d15abb36f513d99fd3bd33d2e330f4dec11dcdb3
neurerohan <nyureron@gmail.com>
2025-05-10 03:16:52 +0545

    yr

--- server/vite.ts
+++ server/vite.ts
@@ -68,18 +68,29 @@ export async function setupVite(app: Express, server: Server) {
 }
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(import.meta.dirname, "public");
+  const distPath = path.resolve(import.meta.dirname, "..", "dist", "public");
+  log(`Serving static files from: ${distPath}`, "serveStatic");
 
   if (!fs.existsSync(distPath)) {
-    throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`,
-    );
+    const errorMsg = `Could not find the build directory: ${distPath}. Make sure the client has been built and the path is correct.`;
+    log(errorMsg, "serveStatic-ERROR");
+    throw new Error(errorMsg);
   }
+  log(`Found 'dist/public' directory at: ${distPath}`, "serveStatic");
 
   app.use(express.static(distPath));
+  log(`express.static middleware configured for ${distPath}`, "serveStatic");
 
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+    const indexPath = path.resolve(distPath, "index.html");
+    log(`Attempting to serve index.html from: ${indexPath}`, "serveStatic");
+    if (fs.existsSync(indexPath)) {
+      res.sendFile(indexPath);
+    } else {
+      const notFoundMsg = `index.html not found at ${indexPath}`;
+      log(notFoundMsg, "serveStatic-ERROR");
+      res.status(404).send(notFoundMsg);
+    }
   });
 }
